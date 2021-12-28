45c1f315b793794395979198c3ceeb8c0aa4bc71
proj97 <90760385+proj97@users.noreply.github.com>
2021-12-28 13:20:01 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,55 @@
 var dataSet = [
-    ["1. ofijsd"],
-    ["2. ofijsd"],
-    ["<a href='aaa' class='btn'>3. ofijsd</a>"]
+    ["<a href='https://shp.ee/ntntv5d' class='btn'>1. Busa Penutup Celah Lubang Bawah Alas Pintu Penghalang Debu Binatang Suara Anti Kecoa</a>"],
+    ["<a href='https://shp.ee/ftd79td' class='btn'>2. TUTU Penghancur Bawang SLAP CHOP Pencacah Bumbu Dapur Manual Onion Chopper Blender Manual. </a>"],
+    ["<a href='https://shp.ee/uyhiz9d' class='btn'>3. Pet Roller Bulu Kucing tidak perlu refill Stick roller </a>"],
+    ["<a href='https://shp.ee/7823dzd' class='btn'>4. TRAVEL POUCH ORGANIZER AC47 SERBAGUNA POUCH SERUT # Bisa COD </a>"],
+    ["<a href='https://shp.ee/nqzb5rd' class='btn'>5. Bel Rumah Wireless Door Bell Pintu Doorbell Waterproof  </a>"],
+    ["<a href='https://shp.ee/665cxxd' class='btn'>6. Taplak Meja Makan Waterproof Anti Air Cover Bahan Peva </a>"],
+    ["<a href='https://shp.ee/udkettd' class='btn'>7. Stick Masker Green Tea Pembersih Pori-Pori / Kotoran Untuk Perawatan</a>"],
+    ["<a href='https://shp.ee/c9xmpfd' class='btn'>8. Tas Ransel Hewan atau Travel Bag Nyaman dan Anti Air Cat Bag Pet Bag</a>"],
+    ["<a href='https://shp.ee/j2j75sd' class='btn'>9. Boneka Santa dan Rusa Ukuran 40 cm</a>"],
+    ["<a href='https://shp.ee/xzru3rd' class='btn'>10. Saringan Saluran Wastafel</a>"],
+    ["<a href='https://shp.ee/7ibh7md' class='btn'>11. Keranjang Baju Gantung Laundry Lipat Tempat Baju Kotor </a>"],
+    ["<a href='https://shp.ee/7ibh7md' class='btn'>12. Tempat Tisue Mobil Boneka Monyet Gantung Karakter </a>"],
+    ["<a href='https://shp.ee/c7tmk5d' class='btn'>13. Mesin Jahit Mini Portable</a>"],
+    ["<a href='https://shp.ee/yusvx7d' class='btn'>14. Mainan steamer BAKPAO DIMSUM SQUISHY </a>"],
+    ["<a href='https://shp.ee/ktwby7d' class='btn'>15. Non slip silicone kacamata</a>"],
+    ["<a href='https://shp.ee/jnwmh7d' class='btn'>16. CETAKAN BAKPAO BAKPAU</a>"],
+    ["<a href='https://shp.ee/gsvvrjd' class='btn'>17. Gelas Sikat Gigi Kumur Tempat Odol </a>"],
+    ["<a href='https://shp.ee/y8e8tvd' class='btn'>18. Alat Pembersih Pengupas Pengikis Sisik Ikan</a>"],
+    ["<a href='https://shp.ee/pfkt8md' class='btn'>19. Sikat Pembersih Botol Susu Termos Penutup Botol Multifungsi </a>"],
+    ["<a href='https://shp.ee/zsjwqvd' class='btn'>20. Mesin Pengupas  Kuaci</a>"],
+    ["<a href='https://shp.ee/r8zbp5d' class='btn'>21. Tempat Sikat Gigi dan Odol</a>"],
+    ["<a href='https://shp.ee/vpmfhvd' class='btn'>22. Kotak Sekat Pembatas Pakaian</a>"],
+    ["<a href='https://shp.ee/z6q3epd' class='btn'>23. Botol Minyak Saus Kecap Bahan Plastik Silikon Anti Tumpah</a>"],
+    ["<a href='https://shp.ee/83cq77d' class='btn'>24. TOPLES BUMBU DAPUR</a>"],
+    ["<a href='https://shp.ee/56fnm7d' class='btn'>25. Keranjang Tong Sampah Lipat Gantung Portable</a>"],
+    ["<a href='https://shp.ee/y6cbapd' class='btn'>26. Panci Listrik Serbaguna</a>"],
+    ["<a href='https://shp.ee/3uir8fd' class='btn'>27. ALAT PRESS BAWANG PUTIH STAINLESS STEEL PENGGERUS</a>"],
+    ["<a href='https://shp.ee/xvrx9ad' class='btn'>28. Alas Ketiak/Pembalut Ketiak</a>"],
+    ["<a href='https://shp.ee/qzvrkad' class='btn'>29. TUDUNG SAJI ELEKTRIK PENUTUP</a>"],
+    ["<a href='https://shp.ee/9mjuwsd' class='btn'>30. Alat Pemotong Buah dan Sayur Serbaguna</a>"],
+    ["<a href='https://shp.ee/fv2pn6d' class='btn'>31. kotak penyimpanan pakaian serbaguna</a>"],
+    ["<a href='https://shp.ee/kw29qnd' class='btn'>32. Cetakan Bakso Sendok</a>"],
+    ["<a href='https://shp.ee/bhtbrcd' class='btn'>33. Tempat Sabun Batang</a>"],
+    ["<a href='https://shp.ee/yavjm4d' class='btn'>34. DOUBLE SIDED TAPE TRANSPARAN</a>"],
+    ["<a href='https://shp.ee/itjdqcd' class='btn'>35. SENDOK KUAH PEMISAH MINYAK LEMAK SUP STAINLESS STEEL</a>"],
+    ["<a href='https://shp.ee/nqhga2d' class='btn'>36. Zipper Pouch Tempat Penyimpanan</a>"],
+    ["<a href='https://shp.ee/6bkw6nd' class='btn'>37. tempat sabun batang</a>"],
+    ["<a href='https://shp.ee/r2nf96d' class='btn'>38. KURSI LIPAT PORTABLE</a>"],
+    ["<a href='https://shp.ee/4h7cssd' class='btn'>39. Baskom pencuci atau Pengering Sayuran Buah Saringan Multifungsi 6in1 Peniris Air</a>"],
+    ["<a href='https://shp.ee/746fjed' class='btn'>40. Rak Sendal Minimalis Gantungan Tempat Sendal</a>"],
+    ["<a href='https://shp.ee/4tx27gd' class='btn'>41. TEMPAT SAMPAH MOBIL TELESCOPIC</a>"],
+    ["<a href='https://shp.ee/unyevud' class='btn'>42. tikar lipat kekinian</a>"],
+    ["<a href='https://shp.ee/7ezbxud' class='btn'>43. Tatakan Gelas Pemanas Coffee</a>"],
+    ["<a href='https://shp.ee/ribwrud' class='btn'>44. Botol Minum Anak Donut</a>"],
+    ["<a href='https://shp.ee/fj7fskd' class='btn'>45. Rak Mainan Anak Rak Susun</a>"],
+    ["<a href='https://shp.ee/gxwn5wd' class='btn'>46. Pembersih Telinga Korek Kuping Ear Wax Picker 1 Set 7 PCS Stainless Stee</a>"],
+    ["<a href='https://shp.ee/yi5y6nd' class='btn'>47. Pompa Galon Elektrik LED / Non LED USB Rechargeable Dispenser Air Minum</a>"],
+    ["<a href='https://shp.ee/tjr8ksd' class='btn'>48. Botol Minum 2 Liter Botol Minum Viral  </a>"],
+    ["<a href='https://shp.ee/9xr4end' class='btn'>49. Alat Pres Plastik Pembungkus Makanan Mini Hand Heat Sealer</a>"],
+    ["<a href='https://shp.ee/373y7ed' class='btn'>50. Papan Cuci Baju Alas Pencuci</a>"],
+    
     
 ];
  
@@ -9,8 +57,9 @@ $(document).ready(function() {
     $('#example').DataTable( {
         data: dataSet,
         columns: [
-            { title: "PRODUK" },
-            
-        ]
+            { title: "PRODUK" },   
+        ],
+        order:[]
+
     } );
 } );
